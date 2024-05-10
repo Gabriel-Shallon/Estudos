bfa776a1d157f2aa6f717934f1556af04b961efd
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2024-05-10 13:56:47 -0400

    ta em work ainda

---
+++ Conceitos/Estrutura de Dados/013_Vetores_Estáticos/Vetores.c
@@ -0,0 +1,15 @@
+A forma mais simples de estruturarmos uma >lista de elementos< é por meio de >vetores/array<
+
+EXEMPLO:
+/////////////////////////////////////////////
+
+int v[10];
+
+Nome: V;
+Quantidade de Elementos no vetor: 10;
+Tipo dos elementos dentro do vetor: int;
+
+- Um vetor de inteiros dimensionado com 10 elementos;
+- Reservamos um espaço de memória contínuo para armazenar 10 valores inteiros;
+- Se cada int ocupa 4 bytes, a declaração reserva um espaço de >40 bytes<;
+
